0455ed912a9a011ffbad9272764404c13aab5059
HeitoAlex <giallorossotuttalavita@hotmail.it>
2024-08-20 16:51:24 +0200

    create rotte api e controller per gestire le risorse

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+
+    public function index(){
+
+        $projects = Project::with( 'type', 'tags')->paginate(20);
+
+        return response()->json([
+            'success' => true,
+            'results' => $projects
+        ]);
+
+    }
+
+    public function show(Project $project){
+
+        $project->loadMissing( 'type', 'tags');
+
+        return response()->json([
+            'success' => true,
+            'results' => $project
+        ]);
+
+    }
+}
